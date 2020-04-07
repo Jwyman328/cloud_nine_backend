008916b13b6977b7cdea306c171b63757f46ceef
Joseph Wyman <jwymandev@gmail.com>
2020-04-07 19:30:14 -0400

    Add sserializer

--- main/urls.py
+++ main/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from . import views
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('main.urls')),
+    path('latest_news_items/', views.LatestNewsItems.as_view(), name='latest_news_items'),
+
 
 ]
--- main/views.py
+++ main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import NewsItem
+from .serializers import LastestNewsItemsSerializer
 # Create your views here.
+class LatestNewsItems(views.APIView):
+
+    def get(self, request):
+        user = request.user
+        lastFiveNewsItems = NewsItem.objects.all()[:5]
+        serialized_data = LastestNewsItemsSerializer(lastFiveNewsItems,  many=True).data
+        return Response(serialized_data, status.HTTP_200_OK)
