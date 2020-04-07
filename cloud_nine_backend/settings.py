5a9f9b40cd28a86b89801ce9ea8ed9e96703a573
Joseph Wyman <jwymandev@gmail.com>
2020-04-07 19:50:29 -0400

    Add local host to white list

--- cloud_nine_backend/settings.py
+++ cloud_nine_backend/settings.py
@@ -124,7 +124,8 @@ AUTH_PASSWORD_VALIDATORS = [
 
 CORS_ORIGIN_WHITELIST = [
     "http://localhost:8080",
-    "http://127.0.0.1:9000"
+    "http://127.0.0.1:9000",
+    "http://localhost:3003",
 ]
 
 
