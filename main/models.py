f6cf3dd9cb99bbe656914bf23026707cb6e40528
Joseph Wyman <jwymandev@gmail.com>
2020-04-08 09:34:20 -0400

    Fix error

--- main/models.py
+++ main/models.py
@@ -7,7 +7,7 @@ class NewsItem(models.Model):
     photo_url = models.TextField(blank=True, null=True)
     date_published = models.DateTimeField(auto_now=True)
 
-class TourItem(modelsModel):
+class TourItem(models.Model):
     date = models.DateTimeField(blank=True, null=True)
     location = models.CharField(max_length=300, blank=True, null=True)
     venue = models.CharField(max_length=300, blank=True, null=True)
